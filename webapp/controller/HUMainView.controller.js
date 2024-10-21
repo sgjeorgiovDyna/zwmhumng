ce32d2d86635e527e9ba27fbd6e916159ce14a98
Stojan Gjeorgiov <s.gjeorgiov@dynabrains.com>
2024-10-21 16:40:04 +0200

    Scan document fragment

--- webapp/controller/HUMainView.controller.js
+++ webapp/controller/HUMainView.controller.js
@@ -1,12 +1,16 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller) {
+        "use strict";
 
-    return Controller.extend("zmwhumng.controller.HUMainView", {
-        onInit: function () {
+        return Controller.extend("zmwhumng.controller.HUMainView", {
+            onInit: function () {
 
-        }
+            },
+            onMVScan: function (oEvent) {
+                var navCon = this.getView().byId("navConMV");
+                navCon.to(this.getView().byId("pageDocScan"));
+            }
+        });
     });
-});
